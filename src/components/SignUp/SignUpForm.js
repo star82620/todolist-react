1c734f1e3b13357044fb300bf5b18f1ee025e778
Ayu Huang <star82620@gmail.com>
2023-08-09 17:07:09 +0800

    update/ adjust PR comment - render TextInput with map

--- src/components/SignUp/SignUpForm.js
+++ src/components/SignUp/SignUpForm.js
@@ -1,16 +1,16 @@
 import TextInput from "../TextInput";
 
 export default function SignUpForm({ signUpState, setSignUpState }) {
+  const postData = { user: signUpState };
+  const apiUrl = "https://todoo.5xcamp.us/users";
+
   //獲取表單資料
   function catchSignUpData(e) {
     signUpState[e.target.name] = e.target.value;
     setSignUpState(signUpState);
-    console.log(Object.values(signUpState));
   }
 
   function submitSignUp() {
-    const postData = { user: signUpState };
-    const apiUrl = "https://todoo.5xcamp.us/users";
     fetch(apiUrl, {
       method: "POST",
       headers: { "Content-Type": "application/json" },
@@ -28,33 +28,45 @@ export default function SignUpForm({ signUpState, setSignUpState }) {
       .catch((error) => console.log(error));
   }
 
+  const formAry = [
+    {
+      label: "Email",
+      placeholder: "請輸入Email",
+      name: "email",
+      changeFunc: catchSignUpData,
+    },
+    {
+      label: "您的暱稱",
+      placeholder: "請輸入您的暱稱",
+      name: "nickname",
+      changeFunc: catchSignUpData,
+    },
+    {
+      label: "密碼",
+      placeholder: "請輸入密碼",
+      name: "password",
+      changeFunc: catchSignUpData,
+    },
+    {
+      label: "再次輸入密碼",
+      placeholder: "請再次輸入密碼",
+      name: "rePassword",
+      changeFunc: catchSignUpData,
+    },
+  ];
+
   return (
     <form className="flex flex-col items-center">
       <div className="w-full flex flex-col gap-y-4 py-6 self-start">
-        <TextInput
-          label="Email"
-          placeholder="請輸入Email"
-          name="email"
-          changeFunc={catchSignUpData}
-        />
-        <TextInput
-          label="您的暱稱"
-          placeholder="請輸入您的暱稱"
-          name="nickname"
-          changeFunc={catchSignUpData}
-        />
-        <TextInput
-          label="密碼"
-          placeholder="請輸入密碼"
-          name="password"
-          changeFunc={catchSignUpData}
-        />
-        <TextInput
-          label="再次輸入密碼"
-          placeholder="請再次輸入密碼"
-          name="rePassword"
-          // changeFunc={confirmPassword}
-        />
+        {formAry.map((item) => (
+          <TextInput
+            key={item.name}
+            label={item.label}
+            placeholder={item.placeholder}
+            name={item.name}
+            changeFunc={item.changeFunc}
+          />
+        ))}
       </div>
       <button
         className="rounded-[10px] px-12 py-3 my-0 mx-auto bg-black text-white"
