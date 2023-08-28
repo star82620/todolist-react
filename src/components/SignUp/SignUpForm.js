2db56ee2559212e1f07ae511375c88b998594ca5
Ayu Huang <star82620@gmail.com>
2023-08-28 10:31:42 +0800

    feature/ after signup success -> tasks

--- src/components/SignUp/SignUpForm.js
+++ src/components/SignUp/SignUpForm.js
@@ -3,39 +3,6 @@ import TextInput from "../TextInput";
 
 export default function SignUpForm({ signUpState, setSignUpState }) {
   const navigate = useNavigate();
-  const postData = { user: signUpState };
-  const apiUrl = "https://todoo.5xcamp.us/users";
-
-  //獲取表單資料
-  function catchSignUpData(e) {
-    signUpState[e.target.name] = e.target.value;
-    setSignUpState(signUpState);
-  }
-
-  async function submitSignUp() {
-    try {
-      const res = await fetch(apiUrl, {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(postData),
-      });
-      const headers = await res.headers;
-      const token = await headers.get("authorization");
-      localStorage.setItem("userToken", await token);
-      const data = await res.json();
-      console.log(data);
-      if (res.ok) {
-        alert("註冊成功，將跳轉至任務頁面");
-        navigate("/tasks");
-      } else {
-        const errMsg = data.error;
-        console.log(errMsg);
-      }
-    } catch (err) {
-      console.log(err);
-    }
-  }
-
   const formAry = [
     {
       label: "Email",
@@ -63,6 +30,39 @@ export default function SignUpForm({ signUpState, setSignUpState }) {
     },
   ];
 
+  //獲取表單資料
+  function catchSignUpData(e) {
+    signUpState[e.target.name] = e.target.value;
+    setSignUpState(signUpState);
+  }
+
+  async function submitSignUp() {
+    //驗證後才敲 API
+    const postData = { user: signUpState };
+    const apiUrl = "https://todoo.5xcamp.us/users";
+    try {
+      const res = await fetch(apiUrl, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(postData),
+      });
+      const headers = await res.headers;
+      const token = await headers.get("authorization");
+      localStorage.setItem("userToken", await token);
+      const data = await res.json();
+      console.log(data);
+      if (res.ok) {
+        alert("註冊成功，將跳轉至任務頁面");
+        navigate("/tasks");
+      } else {
+        const errMsg = data.error;
+        console.log(errMsg);
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
   return (
     <form className="flex flex-col items-center">
       <div className="w-full flex flex-col gap-y-4 py-6 self-start">
@@ -73,6 +73,7 @@ export default function SignUpForm({ signUpState, setSignUpState }) {
             placeholder={item.placeholder}
             name={item.name}
             changeFunc={item.changeFunc}
+            errMsg={item.errMsg}
           />
         ))}
       </div>
